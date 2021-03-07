17d3aabab5fa89c05bdef9e5d1cfe4939056e9b1
Öykü Nehir Şüyün <suyunoyku@gmail.com>
2021-03-07 20:56:58 +0300

    initial commit

---
+++ BauPhi/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  BauPhi
+//
+//  Created by oyku suyun on 7.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ BauPhi/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BauPhi
+//
+//  Created by oyku suyun on 7.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
