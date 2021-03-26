77e06ed0987a7b1fceb9ee6bf5ab25f106cb5f2b
Öykü Nehir Şüyün <suyunoyku@gmail.com>
2021-03-26 13:33:48 +0300

    Page for add event and house initialized

---
+++ fal/Tab Bar Controllers/AddEventViewController.h
@@ -0,0 +1,17 @@
+//
+//  AddEventViewController.h
+//  BauPhi
+//
+//  Created by oyku suyun on 26.03.2021.
+//  Copyright © 2021 Öykü. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddEventViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ fal/Tab Bar Controllers/AddEventViewController.m
@@ -0,0 +1,32 @@
+//
+//  AddEventViewController.m
+//  BauPhi
+//
+//  Created by oyku suyun on 26.03.2021.
+//  Copyright © 2021 Öykü. All rights reserved.
+//
+
+#import "AddEventViewController.h"
+
+@interface AddEventViewController ()
+
+@end
+
+@implementation AddEventViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
